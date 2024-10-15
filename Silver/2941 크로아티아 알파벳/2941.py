99b42321726e1414e1301c0b8c0537721ccb5c7a
younguk2 <neostory1515@gmail.com>
2024-10-15 16:46:34 +0900

    add: 2941  크로아티아 알파뱃 문제

---
+++ Silver/2941 크로아티아 알파벳/2941.py
@@ -0,0 +1,13 @@
+inStr=list(map(str,input()))
+cnt=0
+for i in range(len(inStr)-1,-1,-1):
+    if(ord(inStr[i])>=97 and ord(inStr[i])<=122):
+        cnt+=1
+    if inStr[i]=="=":
+        if inStr[i-1]=="z":
+            if inStr[i-2]=="d":
+                cnt-=1
+    elif inStr[i]=="j":
+        if inStr[i-1]=="n" or inStr[i-1]=="l":
+            cnt-=1
+print(str(cnt),sep="\n")
